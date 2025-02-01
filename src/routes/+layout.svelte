f22e75888dad1488c6c39520db0abfa3acce51a4
telmenb <tbayar2@illinois.edu>
2025-01-31 23:00:15 -0600

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="h-screen flex flex-col items-center justify-center bg-pink-100">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+  import Query from "./Query.svelte";
+
+  let clickedYes = $state(false);
+
+  function yesClicked() {
+    clickedYes = true;
+  }
+</script>
+
+{#if !clickedYes}
+  <Query {yesClicked} />
+{:else}
+  <p>let's goo!</p>
+{/if}
